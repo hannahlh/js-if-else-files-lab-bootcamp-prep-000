8ce9511cb603bde897df2d4c718d1dd57d88c19c
Hannah Hughes <hugheshannah96@gmail.com>
2018-05-22 22:40:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ var height = "74"
 module.exports = { name, height
 }
 
-if 
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+
+if (height === "70") {
+  console.log("The height is correct")
+} else {
+  console.log("Expected the height to be 70")
+}
